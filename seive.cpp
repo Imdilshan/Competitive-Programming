14f8a0f87c3407ad080d6988af657ffc2d3cfd64
Md Dilshan <158074772+Imdilshan@users.noreply.github.com>
2025-02-25 16:11:46 +0530

    Add files via upload

---
+++ seive.cpp
@@ -0,0 +1,15 @@
+vector<int> primes;
+void sieve(int N) {
+    vector<bool> is_prime(N + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for(int i = 2; i * i <= N; i++) {
+        if(is_prime[i]) {
+            for(int j = i * i; j <= N; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    for(int i = 2; i <= N; i++) {
+        if(is_prime[i]) primes.push_back(i);
+    }
+}
